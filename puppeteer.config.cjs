6c78484a14abfc8e6734c4f3b619df89bb5e3af7
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-08-21 01:09:35 -0400

    fix comandos de instalacion de pupeteer

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -4,6 +4,5 @@ const path = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // 🎯 CORRECCIÓN: path.resolve garantiza que la ruta sea absoluta desde la raíz del servidor
   cacheDirectory: path.resolve(__dirname, '.cache', 'puppeteer'),
 };
